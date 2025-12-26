eb4f751d55bdaeb669b61d6a70bf6735b8eacf9c
josscdev <yeicc3000@gmail.com>
2025-12-25 23:50:20 -0500

    feat: archivos

---
+++ astro.config.mjs
@@ -0,0 +1,13 @@
+// // @ts-check
+// import { defineConfig } from 'astro/config';
+
+// // https://astro.build/config
+// export default defineConfig({});
+
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  site: 'https://joscdev.github.io',
+  base: '/resume/',
+});
+
